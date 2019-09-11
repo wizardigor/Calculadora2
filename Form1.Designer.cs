79f48bda317b79e6a71145193f9dcda89f3dcfe6
wizardigor <igorgomesads@hotmail.com>
2019-09-11 02:41:04 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Calculapdora
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtVisor = new System.Windows.Forms.TextBox();
+            this.btnSete = new System.Windows.Forms.Button();
+            this.btnOito = new System.Windows.Forms.Button();
+            this.btnNove = new System.Windows.Forms.Button();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.btnQuatro = new System.Windows.Forms.Button();
+            this.btnCinco = new System.Windows.Forms.Button();
+            this.btnSeis = new System.Windows.Forms.Button();
+            this.btnMultiplicacao = new System.Windows.Forms.Button();
+            this.btnUm = new System.Windows.Forms.Button();
+            this.btnDois = new System.Windows.Forms.Button();
+            this.btnSubtracao = new System.Windows.Forms.Button();
+            this.btnTres = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnPonto = new System.Windows.Forms.Button();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.txtVisor2 = new System.Windows.Forms.TextBox();
+            this.btnEsc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtVisor
+            // 
+            this.txtVisor.Location = new System.Drawing.Point(13, 31);
+            this.txtVisor.Name = "txtVisor";
+            this.txtVisor.Size = new System.Drawing.Size(194, 20);
+            this.txtVisor.TabIndex = 0;
+            // 
+            // btnSete
+            // 
+            this.btnSete.Location = new System.Drawing.Point(13, 67);
+            this.btnSete.Name = "btnSete";
+            this.btnSete.Size = new System.Drawing.Size(34, 23);
+            this.btnSete.TabIndex = 1;
+            this.btnSete.Text = "7";
+            this.btnSete.UseVisualStyleBackColor = true;
+            this.btnSete.Click += new System.EventHandler(this.BtnSete_Click);
+            // 
+            // btnOito
+            // 
+            this.btnOito.Location = new System.Drawing.Point(53, 67);
+            this.btnOito.Name = "btnOito";
+            this.btnOito.Size = new System.Drawing.Size(34, 23);
+            this.btnOito.TabIndex = 2;
+            this.btnOito.Text = "8";
+            this.btnOito.UseVisualStyleBackColor = true;
+            this.btnOito.Click += new System.EventHandler(this.BtnOito_Click);
+            // 
+            // btnNove
+            // 
+            this.btnNove.Location = new System.Drawing.Point(93, 67);
+            this.btnNove.Name = "btnNove";
+            this.btnNove.Size = new System.Drawing.Size(34, 23);
+            this.btnNove.TabIndex = 3;
+            this.btnNove.Text = "9";
+            this.btnNove.UseVisualStyleBackColor = true;
+            this.btnNove.Click += new System.EventHandler(this.BtnNove_Click);
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.Location = new System.Drawing.Point(133, 67);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(34, 23);
+            this.btnDivisao.TabIndex = 4;
+            this.btnDivisao.Text = "/";
+            this.btnDivisao.UseVisualStyleBackColor = true;
+            this.btnDivisao.Click += new System.EventHandler(this.BtnDivisao_Click);
+            // 
+            // btnQuatro
+            // 
+            this.btnQuatro.Location = new System.Drawing.Point(12, 96);
+            this.btnQuatro.Name = "btnQuatro";
+            this.btnQuatro.Size = new System.Drawing.Size(34, 23);
+            this.btnQuatro.TabIndex = 5;
+            this.btnQuatro.Text = "4";
+            this.btnQuatro.UseVisualStyleBackColor = true;
+            this.btnQuatro.Click += new System.EventHandler(this.BtnQuatro_Click);
+            // 
+            // btnCinco
+            // 
+            this.btnCinco.Location = new System.Drawing.Point(53, 96);
+            this.btnCinco.Name = "btnCinco";
+            this.btnCinco.Size = new System.Drawing.Size(34, 23);
+            this.btnCinco.TabIndex = 6;
+            this.btnCinco.Text = "5";
+            this.btnCinco.UseVisualStyleBackColor = true;
+            this.btnCinco.Click += new System.EventHandler(this.BtnCinco_Click);
+            // 
+            // btnSeis
+            // 
+            this.btnSeis.Location = new System.Drawing.Point(93, 96);
+            this.btnSeis.Name = "btnSeis";
+            this.btnSeis.Size = new System.Drawing.Size(34, 23);
+            this.btnSeis.TabIndex = 7;
+            this.btnSeis.Text = "6";
+            this.btnSeis.UseVisualStyleBackColor = true;
+            this.btnSeis.Click += new System.EventHandler(this.BtnSeis_Click);
+            // 
+            // btnMultiplicacao
+            // 
+            this.btnMultiplicacao.Location = new System.Drawing.Point(133, 96);
+            this.btnMultiplicacao.Name = "btnMultiplicacao";
+            this.btnMultiplicacao.Size = new System.Drawing.Size(34, 23);
+            this.btnMultiplicacao.TabIndex = 8;
+            this.btnMultiplicacao.Text = "*";
+            this.btnMultiplicacao.UseVisualStyleBackColor = true;
+            this.btnMultiplicacao.Click += new System.EventHandler(this.BtnMultiplicacao_Click);
+            // 
+            // btnUm
+            // 
+            this.btnUm.Location = new System.Drawing.Point(13, 125);
+            this.btnUm.Name = "btnUm";
+            this.btnUm.Size = new System.Drawing.Size(34, 23);
+            this.btnUm.TabIndex = 9;
+            this.btnUm.Text = "1";
+            this.btnUm.UseVisualStyleBackColor = true;
+            this.btnUm.Click += new System.EventHandler(this.BtnUm_Click);
+            // 
+            // btnDois
+            // 
+            this.btnDois.Location = new System.Drawing.Point(53, 125);
+            this.btnDois.Name = "btnDois";
+            this.btnDois.Size = new System.Drawing.Size(34, 23);
+            this.btnDois.TabIndex = 10;
+            this.btnDois.Text = "2";
+            this.btnDois.UseVisualStyleBackColor = true;
+            this.btnDois.Click += new System.EventHandler(this.BtnDois_Click);
+            // 
+            // btnSubtracao
+            // 
+            this.btnSubtracao.Location = new System.Drawing.Point(133, 125);
+            this.btnSubtracao.Name = "btnSubtracao";
+            this.btnSubtracao.Size = new System.Drawing.Size(34, 23);
+            this.btnSubtracao.TabIndex = 11;
+            this.btnSubtracao.Text = "-";
+            this.btnSubtracao.UseVisualStyleBackColor = true;
+            this.btnSubtracao.Click += new System.EventHandler(this.BtnSubtracao_Click);
+            // 
+            // btnTres
+            // 
+            this.btnTres.Location = new System.Drawing.Point(93, 125);
+            this.btnTres.Name = "btnTres";
+            this.btnTres.Size = new System.Drawing.Size(34, 23);
+            this.btnTres.TabIndex = 12;
+            this.btnTres.Text = "3";
+            this.btnTres.UseVisualStyleBackColor = true;
+            this.btnTres.Click += new System.EventHandler(this.BtnTres_Click);
+            // 
+            // btnZero
+            // 
+            this.btnZero.Location = new System.Drawing.Point(13, 154);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(74, 23);
+            this.btnZero.TabIndex = 13;
+            this.btnZero.Text = "0";
+            this.btnZero.UseVisualStyleBackColor = true;
+            this.btnZero.Click += new System.EventHandler(this.BtnZero_Click);
+            // 
+            // btnPonto
+            // 
+            this.btnPonto.Location = new System.Drawing.Point(93, 154);
+            this.btnPonto.Name = "btnPonto";
+            this.btnPonto.Size = new System.Drawing.Size(34, 23);
+            this.btnPonto.TabIndex = 15;
+            this.btnPonto.Text = ".";
+            this.btnPonto.UseVisualStyleBackColor = true;
+            this.btnPonto.Click += new System.EventHandler(this.BtnPonto_Click);
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(133, 154);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(34, 23);
+            this.btnSoma.TabIndex = 16;
+            this.btnSoma.Text = "+";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.BtnSoma_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(173, 96);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(34, 23);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Location = new System.Drawing.Point(173, 125);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(34, 52);
+            this.btnIgual.TabIndex = 18;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.BtnIgual_Click);
+            // 
+            // txtVisor2
+            // 
+            this.txtVisor2.Location = new System.Drawing.Point(13, 12);
+            this.txtVisor2.Name = "txtVisor2";
+            this.txtVisor2.Size = new System.Drawing.Size(194, 20);
+            this.txtVisor2.TabIndex = 19;
+            // 
+            // btnEsc
+            // 
+            this.btnEsc.Location = new System.Drawing.Point(173, 67);
+            this.btnEsc.Name = "btnEsc";
+            this.btnEsc.Size = new System.Drawing.Size(34, 23);
+            this.btnEsc.TabIndex = 20;
+            this.btnEsc.Text = "Esc";
+            this.btnEsc.UseVisualStyleBackColor = true;
+            this.btnEsc.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 194);
+            this.Controls.Add(this.btnEsc);
+            this.Controls.Add(this.txtVisor2);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.btnPonto);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.btnTres);
+            this.Controls.Add(this.btnSubtracao);
+            this.Controls.Add(this.btnDois);
+            this.Controls.Add(this.btnUm);
+            this.Controls.Add(this.btnMultiplicacao);
+            this.Controls.Add(this.btnSeis);
+            this.Controls.Add(this.btnCinco);
+            this.Controls.Add(this.btnQuatro);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.btnNove);
+            this.Controls.Add(this.btnOito);
+            this.Controls.Add(this.btnSete);
+            this.Controls.Add(this.txtVisor);
+            this.Name = "frmCalculadora";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVisor;
+        private System.Windows.Forms.Button btnSete;
+        private System.Windows.Forms.Button btnOito;
+        private System.Windows.Forms.Button btnNove;
+        private System.Windows.Forms.Button btnDivisao;
+        private System.Windows.Forms.Button btnQuatro;
+        private System.Windows.Forms.Button btnCinco;
+        private System.Windows.Forms.Button btnSeis;
+        private System.Windows.Forms.Button btnMultiplicacao;
+        private System.Windows.Forms.Button btnUm;
+        private System.Windows.Forms.Button btnDois;
+        private System.Windows.Forms.Button btnSubtracao;
+        private System.Windows.Forms.Button btnTres;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnPonto;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.TextBox txtVisor2;
+        private System.Windows.Forms.Button btnEsc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculapdora
+{
+    public partial class frmCalculadora : Form
+    {
+        bool ponto = false;
+        bool operador = false;
+        int tamText2 = 0;
+        double acumulador = 0;
+        int sinal = 4;
+
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnZero_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "1";
+            }
+            else
+            {
+                txtVisor.Text += "0";
+            }
+          
+        }
+
+        private void BtnUm_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "1";
+            }
+            else
+            {
+                txtVisor.Text += "1";
+            }
+        }
+
+        private void BtnDois_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "2";
+            }
+            else
+            {
+                txtVisor.Text += "2";
+            }
+        }
+
+        private void BtnTres_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "3";
+            }
+            else
+            {
+                txtVisor.Text += "3";
+            }
+        }
+
+        private void BtnQuatro_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "4";
+            }
+            else
+            {
+                txtVisor.Text += "4";
+            }
+        }
+
+        private void BtnCinco_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "5";
+            }
+            else
+            {
+                txtVisor.Text += "5";
+            }
+        }
+
+        private void BtnSeis_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "6";
+            }
+            else
+            {
+                txtVisor.Text += "6";
+            }
+        }
+
+        private void BtnSete_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "7";
+            }
+            else
+            {
+                txtVisor.Text += "7";
+            }
+        }
+
+        private void BtnOito_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "8";
+            }
+            else
+            {
+                txtVisor.Text += "8";
+            }
+        }
+
+        private void BtnNove_Click(object sender, EventArgs e)
+        {
+            if (operador == true)
+            {
+                txtVisor.Clear();
+                operador = false;
+                txtVisor.Text += "9";
+            }
+            else
+            {
+                txtVisor.Text += "9";
+            }
+        }
+
+        private void BtnPonto_Click(object sender, EventArgs e)
+        {
+            if (ponto != true)
+            {
+                txtVisor.Text += ',';
+                ponto = true;
+            }
+            else
+            {
+                MessageBox.Show("Você já usou o '.' (PONTO).", "IMPORTANTE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }               
+        }
+
+        private void BtnSoma_Click(object sender, EventArgs e)
+        {
+            if (txtVisor2.Text == String.Empty && operador == false)
+            {
+                acumulador = Convert.ToDouble(txtVisor.Text);
+                txtVisor2.Text += txtVisor.Text + '+';
+                ponto = false;
+                operador = true;
+                sinal = 0;
+
+                btnSoma.Enabled = false;
+                btnSubtracao.Enabled = false;
+                btnMultiplicacao.Enabled = false;
+                btnDivisao.Enabled = false;
+                btnIgual.Enabled = true;
+
+            }
+            else if (txtVisor2.Text != String.Empty && operador == false){
+                txtVisor2.Text = Convert.ToString(acumulador) + '+';
+                ponto = false;
+                operador = true;
+                sinal = 0;
+
+                btnSoma.Enabled = false;
+                btnSubtracao.Enabled = false;
+                btnMultiplicacao.Enabled = false;
+                btnDivisao.Enabled = false;
+                btnIgual.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("DIGITE ALGUM VALOR E PRESSIONE = (IGUAL).", "IMPORTANTE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }                      
+        }
+
+        private void BtnSubtracao_Click(object sender, EventArgs e)
+        {
+            if (txtVisor2.Text == String.Empty && operador == false)
+            {
+                acumulador = Convert.ToDouble(txtVisor.Text);
+                txtVisor2.Text += txtVisor.Text + '-';
+                ponto = false;
+                operador = true;
+                sinal = 1;
+
+                btnSoma.Enabled = false;
+                btnSubtracao.Enabled = false;
+                btnMultiplicacao.Enabled = false;
+                btnDivisao.Enabled = false;
+                btnIgual.Enabled = true;
+
+            }
+            else if (txtVisor2.Text != String.Empty && operador == false){
+                txtVisor2.Text = Convert.ToString(acumulador) + '-';
+                ponto = false;
+                operador = true;
+                sinal = 1;
+
+                btnSoma.Enabled = false;
+                btnSubtracao.Enabled = false;
+                btnMultiplicacao.Enabled = false;
+                btnDivisao.Enabled = false;
+                btnIgual.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("DIGITE ALGUM VALOR E PRESSIONE = (IGUAL).", "IMPORTANTE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void BtnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            if (txtVisor2.Text == String.Empty && operador == false)
+            {
+                acumulador = Convert.ToDouble(txtVisor.Text);
+                txtVisor2.Text += txtVisor.Text + '*';
+                ponto = false;
+                operador = true;
+                sinal = 2;
+
+                btnSoma.Enabled = false;
+                btnSubtracao.Enabled = false;
+                btnMultiplicacao.Enabled = false;
+                btnDivisao.Enabled = false;
+                btnIgual.Enabled = true;
+
+            }
+            else if (txtVisor2.Text != String.Empty && operador == false){
+                txtVisor2.Text = Convert.ToString(acumulador) + '*';
+                ponto = false;
+                operador = true;
+                sinal = 2;
+
+                btnSoma.Enabled = false;
+                btnSubtracao.Enabled = false;
+                btnMultiplicacao.Enabled = false;
+                btnDivisao.Enabled = false;
+                btnIgual.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("DIGITE ALGUM VALOR E PRESSIONE = (IGUAL).", "IMPORTANTE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void BtnDivisao_Click(object sender, EventArgs e)
+        {
+            if (operador == false)
+            {
+                if(txtVisor2.Text == String.Empty && acumulador != 0)
+                {
+                    acumulador = Convert.ToDouble(txtVisor.Text);
+                    txtVisor2.Text += txtVisor.Text + '/';
+                    ponto = false;
+                    operador = true;
+                    sinal = 3;
+
+                    btnSoma.Enabled = false;
+                    btnSubtracao.Enabled = false;
+                    btnMultiplicacao.Enabled = false;
+                    btnDivisao.Enabled = false;
+                    btnIgual.Enabled = true;
+                }
+                else if (txtVisor2.Text != String.Empty && operador == false){
+                    txtVisor2.Text = Convert.ToString(acumulador) + '/';
+                    ponto = false;
+                    operador = true;
+                    sinal = 3;
+
+                    btnSoma.Enabled = false;
+                    btnSubtracao.Enabled = false;
+                    btnMultiplicacao.Enabled = false;
+                    btnDivisao.Enabled = false;
+                    btnIgual.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("O VALOR DO DIVIDENDO DEVE SER DIFERENTE DE 0 (ZERO).", "IMPORTANTE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("DIGITE ALGUM VALOR E PRESSIONE = (IGUAL) OU Esc PARA UM NOVO CALCULO.", "IMPORTANTE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void BtnIgual_Click(object sender, EventArgs e)
+        { 
+                switch (sinal)
+                {
+                    case 0: //soma
+                        acumulador += Convert.ToDouble(txtVisor.Text);
+                        txtVisor2.Text += txtVisor.Text + '=' + Convert.ToString(acumulador);
+                        ponto = false;
+                        operador = false;
+                        txtVisor.Clear();
+
+                    btnSoma.Enabled = true;
+                    btnSubtracao.Enabled = true;
+                    btnMultiplicacao.Enabled = true;
+                    btnDivisao.Enabled = true;
+                    btnIgual.Enabled = true;
+                    btnClear.Enabled = true;
+                    break;
+
+                    case 1: //subtração
+                        acumulador -= Convert.ToDouble(txtVisor.Text);
+                        txtVisor2.Text += txtVisor.Text + '=' + Convert.ToString(acumulador);
+                        txtVisor.Clear();
+                        ponto = false;
+                        operador = false;
+
+                    btnSoma.Enabled = true;
+                    btnSubtracao.Enabled = true;
+                    btnMultiplicacao.Enabled = true;
+                    btnDivisao.Enabled = true;
+                    btnIgual.Enabled = true;
+                    btnClear.Enabled = true;
+                    break;
+
+                    case 2: //multiplicação
+                        acumulador *= Convert.ToDouble(txtVisor.Text);
+                        txtVisor2.Text += txtVisor.Text + '=' + Convert.ToString(acumulador);
+                        txtVisor.Clear();
+                        ponto = false;
+                        operador = false;
+
+                    btnSoma.Enabled = true;
+                    btnSubtracao.Enabled = true;
+                    btnMultiplicacao.Enabled = true;
+                    btnDivisao.Enabled = true;
+                    btnIgual.Enabled = true;
+                    btnClear.Enabled = true;
+                    break;
+
+                    case 3: //divisão
+                        acumulador /= Convert.ToDouble(txtVisor.Text);
+                        txtVisor2.Text += txtVisor.Text + '=' + Convert.ToString(acumulador);
+                        txtVisor.Clear();
+                        ponto = false;
+                        operador = false;
+
+                    btnSoma.Enabled = true;
+                    btnSubtracao.Enabled = true;
+                    btnMultiplicacao.Enabled = true;
+                    btnDivisao.Enabled = true;
+                    btnIgual.Enabled = true;
+                    btnClear.Enabled = true;
+                    break;
+                }
+            
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+                txtVisor.Clear();
+                ponto = false;
+           
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            txtVisor.Clear();
+            txtVisor2.Clear();
+            acumulador = 0;
+            ponto = false;
+            operador = false;
+            sinal = -1;
+
+            btnSoma.Enabled = true;
+            btnSubtracao.Enabled = true;
+            btnMultiplicacao.Enabled = true;
+            btnDivisao.Enabled = true;
+            btnIgual.Enabled = true;
+            btnClear.Enabled = true;
+        }
+    }
+}
